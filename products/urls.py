18d63bd7bff9739913ce006ab17c6e5a1b070215
almas <almasyaseen18@gmail.com>
2024-01-18 21:51:44 +0530

    exp

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,9 @@ from .import views
 
 urlpatterns = [
     path('',views.index,name='expenses'),
+     path('expense_edit/<int:id>',views.expense_edit,name='expense_edit'),
     path('add_expense',views.add_expense,name='add_expense'),
+   
+
     
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.decorators import login_required
 from .import views
+from django.contrib import messages
+from .models import *
 from django.views.decorators.cache import never_cache
 
 # Create your views here.
@@ -8,8 +10,66 @@ from django.views.decorators.cache import never_cache
 @login_required(login_url='/authentication/login')
 @never_cache
 def index(request):
-    return render(request,'expenses/index.html')
+    categories = Category.objects.all()
+    expense = Expense.objects.filter(owner=request.user)
+    context = {
+        'categories':categories,
+        'expense':expense,
+    }
+    return render(request,'expenses/index.html',context)
 
 
 def add_expense(request):
-    return render(request,'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories':categories,
+        'values':request.POST
+    }
+    if request.method=="POST":
+        print("akljklasdjksad")
+        amount = request.POST['amount']
+        description = request.POST['description']
+        if not amount:
+            messages.error(request,'Amount is required')
+            return render(request,'expenses/add_expense.html',context)
+        
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+        
+        
+        if not description:
+            messages.error(request,'Amount is required')
+            return render(request,'expenses/add_expense.html',context)
+        
+        Expense.objects.create(owner=request.user,amount=amount,date=date,category=category,description=description)
+        
+        messages.success(request,'Expense saved successfully')
+        
+        return redirect('expenses')
+            
+    
+    
+    return render(request,'expenses/add_expense.html',context)
+
+
+
+
+
+
+def expense_edit(request,id):
+    expense  =Expense.objects.get(id=id)
+    context = {
+        'expense':expense,
+        'values':expense,
+        
+    }
+    if request.method=="GET":
+        return render(request,'expenses/edit-expense.html',context)
+    
+    else:
+        messages.info(request,'Handling post form')
+        return render(request,'expenses/edit-expense.html',context)
+        
+        
+  
